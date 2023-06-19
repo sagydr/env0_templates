8ce0bf5f52275f02c6af2f6f5582ee10dfdd828d
sag <sagy.drucker@env0.com>
2023-06-19 14:32:00 +0300

    added errors

--- misc/custom-flow-tf-vars/main.tf
+++ misc/custom-flow-tf-vars/main.tf
@@ -1,12 +1,12 @@
 resource "null_resource" "null" {
 }
 
-variable "xxxemail" {
-  xxxtype = string
+variable "email" {
+  type = string
   description = "env variable exported in env0.yml"
   default = "default@domain.com"
 }
 
-ouxxxtput "custom_flow_tf_var" {
+output "custom_flow_tf_var" {
   value = var.email
 }
