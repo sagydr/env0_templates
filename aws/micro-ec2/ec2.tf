a28f6874eaa462291eec5fc9729eed8b32216952
sag <sagy.drucker@env0.com>
2023-08-06 14:37:47 +0300

    tag name

--- aws/micro-ec2/ec2.tf
+++ aws/micro-ec2/ec2.tf
@@ -4,10 +4,10 @@ provider "aws" {
 
 resource "aws_instance" "example_server" {
   ami           = "ami-053b0d53c279acc90"
-#  instance_type = "t3.nano"
-  instance_type = var.instance_type
+  instance_type = "t3.nano"
+#  instance_type = var.instance_type
 
   tags = {
-    Name = "Sagyyyyyyy-forkk22222"
+    Name = "Sagyyyyyyy-forkk22222333"
   }
 }
