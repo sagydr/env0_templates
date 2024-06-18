fcbe08c659c9e8ceb8c9f7a66a99353f2fceadbc
sag <sagy.drucker@env0.com>
2024-06-18 17:33:04 +0300

    new

--- misc/null-resource/main.tf
+++ misc/null-resource/main.tf
@@ -1,5 +1,7 @@
-resource "null_resource" "null3343" {
+resource "null_resource" "null334453" {
 }
-resource "null_resource" "null33432" {
+resource "null_resource" "null3343552" {
 }
 
+resource "null_resource2" "null33431552" {
+}
