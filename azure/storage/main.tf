30da41561e1f0f9edf0699f08854b9ac63a31d7b
sagydr <sagysrael@gmail.com>
2024-12-31 12:02:41 +0200

    Update main.tf

--- azure/storage/main.tf
+++ azure/storage/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "example" {
 }
 
 # Define the storage account
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "sag_example" {
   name                     = "exa${random_string.suffix.result}"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
