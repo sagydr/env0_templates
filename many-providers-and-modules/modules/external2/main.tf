c2dfb1984e24db202b1056082a358c417e4622f2
sag <sagy.drucker@env0.com>
2025-03-18 10:47:23 +0200

    public repos

--- many-providers-and-modules/modules/external2/main.tf
+++ many-providers-and-modules/modules/external2/main.tf
@@ -1,7 +1,7 @@
 module "externalGit" {
-  source = "github.com/hashicorp/example"
+  source = "github.com/env0/terraform-provider-env0"
 }
 
 module "externalBB" {
-  source = "bitbucket.org/hashicorp/terraform-consul-aws"
+  source = "bitbucket.org/env0/approval-policies"
 }
